2bd158a0349f9dd86c84b7d2fce745ee3a440f30
Stefano Mercadante <stefmercadante@gmail.com>
2024-09-12 15:49:51 +0200

    state entity: get entity automatically

--- scripts/states/state_entity.gd
+++ scripts/states/state_entity.gd
@@ -2,9 +2,7 @@ extends BaseState
 ##Base class for all entity states.
 class_name StateEntity
 
-@export var entity_player_id := 0 ##If greater than 0, player with the specified id will be set as entity.
-@export var entity: CharacterEntity ##The entity to apply this state. If left empty and this state is child of a CharacterEntity, that entity will be taken.
-
+var entity: CharacterEntity ##The entity to apply this state. If left empty and this state is child of a CharacterEntity, that entity will be taken.
 var entity_name := ""
 
 func enter() -> void:
@@ -14,8 +12,8 @@ func enter() -> void:
 		entity_name = entity.name
 
 func _try_to_get_entity(node):
-	if entity_player_id > 0:
-		return Globals.get_player(entity_player_id)
+	if params.has("entity"):
+		return params["entity"]
 	var parent = node.get_parent()
 	if parent is CharacterEntity:
 		return parent
