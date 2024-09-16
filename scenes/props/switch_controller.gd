431b4ebf78a5b403857c92046a43c1a09e0c4364
Stefano Mercadante <stefmercadante@gmail.com>
2024-09-16 16:14:05 +0200

    add switch, lever and gate

---
+++ scenes/props/switch_controller.gd
@@ -0,0 +1,20 @@
+extends StaticBody2D
+
+@export var interactable: Interactable
+@export var states: Array[BaseState]
+@export var start_state_index := 0
+
+func _ready() -> void:
+	if interactable:
+		interactable.interacted.connect(switch_states)
+
+func switch_states(_sender):
+	var next_state
+	start_state_index += 1
+	if start_state_index < states.size():
+		next_state = states[start_state_index]
+	else:
+		next_state = states[0]
+		start_state_index = 0
+	if next_state:
+		next_state.enable()
