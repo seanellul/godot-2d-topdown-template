57ad50c49b7a8c3d3aa6b53ae25a9e8bb8b368c4
Stefano Mercadante <stefmercadante@gmail.com>
2024-11-22 12:58:21 +0100

    add tutorial 1

--- scripts/state_machine/states/state_input_listener.gd
+++ scripts/state_machine/states/state_input_listener.gd
@@ -11,13 +11,9 @@ func physics_update(_delta):
 		_handle_inputs()
 
 func _handle_inputs():
-	input_dir = Input.get_vector("left", "right", "up", "down")
+	input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if Input.is_action_just_pressed("jump"):
 		entity.jump()
-	# if Input.is_action_just_pressed("attack") and not entity.is_hurting:
-	# 	entity.is_charging = true
-	# if Input.is_action_just_released("attack") and entity.is_charging:
-	# 	entity.is_charging = false
 	if Input.is_action_just_pressed("attack"):
 		entity.attack()
 	var speed_increment = run_speed_increment if Input.get_action_strength("run") > 0 else 1.0 
